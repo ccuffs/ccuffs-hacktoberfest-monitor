d405dde17734f0380ca17392a19f224680947479
Junior V. Ramisch <junior.ramisch@gmail.com>
2020-10-18 00:21:09 -0300

    Adds owners and repos
    It's likely schema will change
    over time, by now it's ok

---
+++ src/data/owners.js
@@ -0,0 +1,19 @@
+export default [
+    {
+        "name": "ccuffs",
+        "repos": [
+            "ideias",
+            "cc.uffs.edu.br",
+            "guia-tcc",
+            "provas-uffs",
+            "app-cc"
+        ]
+    },
+    {
+        "name": "grintex",
+        "repos": [
+            "dados-uffs",
+            "app-covid"
+        ]
+    }
+]
