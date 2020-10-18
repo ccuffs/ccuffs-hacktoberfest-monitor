b4c9e92ba82112691050c1196c5f5ec749c81687
Junior V. Ramisch <junior.ramisch@gmail.com>
2020-10-18 17:04:15 -0300

    feat: creates views list and show

---
+++ src/views/Index.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div class="px-4 pt-4">
+            <div class="d-lg-flex flex-wrap justify-content-center"
+            >
+                <template 
+                    v-for="owner in owners"
+                >
+                    <Repository
+                        v-for="repo in owner.repos"
+                        :key="repo.name"
+                        :name="repo.name"
+                        :owner="owner.name"
+                        :image="repo.image"
+                    />
+
+                    <input type="hidden" :key="owner.name">
+                </template>
+            </div>
+        </div>
+        <TheFooter/>
+    </div>
+</template>
+
+<script>
+
+import Owners from '@/data/owners'
+import Repository from '@/components/Repository'
+
+export default {
+    name: 'Index',
+    components: {
+        Repository
+    },
+    computed: {
+        owners(){
+            return Owners
+        }
+    }
+}
+</script>
+
+<style>
+    .repository:last-of-type {
+        margin-bottom: 0 !important;
+    }
+
+    .justify-content-center {
+        justify-content: center;
+    }
+
+    .orange {
+        color: #ff9900;
+    }
+
+    .light-orange {
+        color: #ffab40;
+    }
+</style>
+
---
+++ src/views/Show.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container-lg clearfix pt-4">
+        <div class="repository-info px-4">
+            <h1>
+                {{ repoName }}
+            </h1>
+        </div>
+        <TheFooter/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ShowRepository',
+    data(){
+        return {
+            repoName: String
+        }
+    },
+    mounted(){
+        this.repoName = this.$route.params.name;
+    }
+}
+</script>
+
+<style>
+    .repository-info{
+        height: 90vh;
+        background-color: #4c4c4c;
+    }
+</style>
